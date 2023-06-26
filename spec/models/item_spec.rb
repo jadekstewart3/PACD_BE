780cc198d3be2d496f40e2677e117758927668de
Chris Crane <chrisjcrane@gmail.com>
2023-06-26 15:22:50 -0600

    correct item_spec and pass

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   context "relationships" do
-    it { should have_many :list_items }
-    it { should have_many(:lists).through(:list_items)}
+    it { should have_many :lists }
   end
 end
