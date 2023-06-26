6c0c75a20977f12c8051ab398687013aec9258f8
Chris Crane <chrisjcrane@gmail.com>
2023-06-26 15:28:29 -0600

    add tests for trip

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "associations" do
+    it { should have_many(:lists) }
+    it { should have_many(:user_trips) }
+    it { should have_many(:users).through(:user_trips) }
+  end
 end
