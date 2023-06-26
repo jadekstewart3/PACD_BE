a930f41512e48813b444df8866dba77509ee94d8
Chris Crane <chrisjcrane@gmail.com>
2023-06-26 15:14:56 -0600

    add relationship test for Item model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "relationships" do
+    it { should have_many :list_items }
+    it { should have_many(:lists).through(:list_items)}
+  end
 end
