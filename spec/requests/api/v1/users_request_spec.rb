43fe3c0bd952f389c78648a772fa6e2d13c28de2
Chris Crane <chrisjcrane@gmail.com>
2023-06-28 11:48:53 -0600

    create test for users index

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
 
-describe "Users API" do
-  
+context "Users API" do
+  describe "GET /api/v1/users" do
+    it "returns all users" do
+      get "/api/v1/users"
+      expect(response).to be_successful
+
+      users = JSON.parse(response.body, symbolize_names: true)
+    end
+  end
 end
