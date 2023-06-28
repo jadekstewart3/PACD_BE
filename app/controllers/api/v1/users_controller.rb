16b173c1055ce7b86c1efee3039bdedfdf535674
Chris Crane <chrisjcrane@gmail.com>
2023-06-28 15:11:15 -0600

    add user#show with passing test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::UsersController < ApplicationController
   def index
     render json: UserSerializer.new(User.all)
   end
+
+  def show
+    render json: UserSerializer.new(User.find(params[:id]))
+  end
 end
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -18,18 +18,19 @@ context "Users API" do
 
   describe "User#Show" do
     it "returns a single user" do
-      get "/api/v1/users/1"
+      get "/api/v1/users/#{@user[:id]}"
       expect(response).to be_successful
-      expect(users[:data].count).to eq(1)
-      expect(users[:data][0][:attributes][:name]).to eq(@user.name)
+
+      users = JSON.parse(response.body, symbolize_names: true)
+      expect(users[:data][:id]).to eq("#{@user.id}")
 
       user2 = create(:user)
-      get "/api/v1/users/2"
+      get "/api/v1/users/#{user2.id}"
 
       expect(response).to be_successful
-      expect(users[:data].count).to eq(1)
-      expect(users[:data][0][:attributes][:name]).to eq(user2.name)
-      expect(users[:data][0][:attributes][:name]).to not_eq(@user.name)
+
+      users = JSON.parse(response.body, symbolize_names: true)
+      expect(users[:data][:id]).to eq("#{user2.id}")
     end
   end
 end
