3657833457bcb52de3c41604bf845d7f1e88ae09
Chris Crane <chrisjcrane@gmail.com>
2023-06-28 11:31:02 -0600

    add validations for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   has_many :list_items
   has_many :lists, through: :list_items
+
+  validates_presence_of :name, :quantity
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Item, type: :model do
   context "relationships" do
     it { should have_many :lists }
   end
+
+  context "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :quantity }
+    # note is optional
+  end
 end
