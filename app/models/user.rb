7509bee16aa1545a230ef320b13fe5691c7a65c2
Chris Crane <chrisjcrane@gmail.com>
2023-06-28 11:40:51 -0600

    add validations for trip and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   has_many :trip, through: :user_trips
   has_many :user_lists
   has_many :lists, through: :user_lists
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  has_secure_password
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Trip, type: :model do
     it { should have_many(:user_trips) }
     it { should have_many(:users).through(:user_trips) }
   end
+
+  context "validations" do
+    it { should validate_presence_of :name }
+  end
 end
