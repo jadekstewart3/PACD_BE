a10ab18ee5455e68be58f0b964706733be5a7f34
Chris Crane <chrisjcrane@gmail.com>
2023-06-29 10:59:20 -0600

    validate numerically of quantity

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
   has_many :lists, through: :list_items
 
   validates_presence_of :name, :quantity
+  validates_numericality_of :quantity, greater_than: 0
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Item, type: :model do
   context "validations" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :quantity }
+    it { should validate_numericality_of(:quantity).is_greater_than(0)}
     # note is optional
   end
 end
